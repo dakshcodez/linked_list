7ef11a244b4ed23dfc8f9bd48b23517b552f14ed
dakshcodez <daksharoracodez@gmail.com>
2025-01-08 09:25:21 +0530

    added count function

--- in C/basic_ops_singly_li_lists.c
+++ in C/basic_ops_singly_li_lists.c
@@ -31,6 +31,15 @@ void insertAtBeg(struct ListNode **head, int val){
     *head = newNode;
 }
 
+int countElements(struct ListNode* head){
+    int count = 0;
+    while (head != NULL){
+        count++;
+        head = head -> next;
+    }
+    return count;
+}
+
 void display(struct ListNode* head){
     while (head != NULL){
         printf("%d -> ", head -> val);
@@ -46,6 +55,9 @@ int main(){
     insertAtEnd(&head, 30);
     insertAtEnd(&head, 40);
     insertAtBeg(&head,1);
+
+    int count = countElements(head);
+    printf("%d\n", count);
     display(head);
     return 0;
 }
