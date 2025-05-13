dc22701e034ed227190bd5a171e79ae143280637
dakshcodez <daksharoracodez@gmail.com>
2025-05-13 16:37:39 +0530

    added delete function

--- in Python/basic_ops.py
+++ in Python/basic_ops.py
@@ -28,6 +28,20 @@ class LinkedList:
             temp = temp.next
         print("None")
 
+    def deleteNode(self, value):
+        if self.head and self.head.data == value:
+            self.head = self.head.next
+            print(f"Node {value} delted")
+            return
+        temp = self.head
+        while temp.next:
+            if temp.next.data == value:
+                temp.next = temp.next.next  
+                print(f"Node {value} delted")
+                return
+            temp = temp.next
+        print("Node not found")
+
 l1 = LinkedList()
 l1.append(1)
 l1.append(2)
@@ -35,4 +49,7 @@ l1.append(3)
 l1.append(4)
 l1.append(5)
 l1.display()
-
+l1.deleteNode(1)
+l1.deleteNode(3)
+l1.deleteNode(5)
+l1.display()
